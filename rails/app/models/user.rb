3921fea0b74de54edbd016c5cd7e3736f0998dea
yojir <yo0102@icloud.com>
2022-06-16 11:40:55 +0900

    add login

---
+++ rails/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+  before_save { self.email = email.downcase } #emailを全部小文字に変換
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8}
+end
