48ed4440a0a8ce07eb2fc7c2dda675c0e8169eed
yojir <yo0102@icloud.com>
2022-06-21 11:27:14 +0900

    [add] siginup

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get    '/signup',   to: 'users#new' 
+  post '/signup',  to: 'users#create'
   resources :users
 end
