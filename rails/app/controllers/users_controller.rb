7c88931b527ae101675c52fdfc737df9ec5a25fc
Shota Minegishi <smngs@mine-313.com>
2022-06-27 23:05:32 +0900

    [BREAKING CHANGE] add `grade, research_theme, introduction` column to `User` table
    * `User` テーブルに対し，新たに `grade, research_theme, introduction`
      カラムを追加しました．
      * 開発環境に対して， `rails db:migrate`
        環境の実行が必要な，破壊的変更です．

--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -19,10 +19,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation,
+        :grade,
+        :research_theme,
+        :introduction,
+      )
     end
   
 end
