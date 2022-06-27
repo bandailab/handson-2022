24627fb765e4c4f4a7882c5c31a574e2dad1982d
Shota Minegishi <smngs@mine-313.com>
2022-06-27 23:15:21 +0900

    [update] create user list (/users/)
    * User 一覧の画面を作成しました．
      * 先に作成した自己紹介文と学年，自身の研究分野を表示するようにしています．
      * プロフィール画像は Gravater から取ってくるようにしました．
        * 画像管理するのがめんどくさいので……

--- rails/app/helpers/users_helper.rb
+++ rails/app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user, size: 80, _class: "gravatar")
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: _class)
+  end
 end
