59e5705917196ab698810bc89c3ce92b9c184755
yojir <yo0102@icloud.com>
2022-06-28 12:10:14 +0900

    [add] user_edit_page

--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -19,6 +19,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+     
+    #編集しようとしてるユーザーがログインユーザーとイコールかをチェック
+    if current_user == @user
+     
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました。'
+        redirect_to @user
+      else
+        flash[:danger] = 'ユーザー情報の編集に失敗しました。'
+        redirect_to edit_user_path
+      end   
+     
+    else
+        redirect_to root_url
+    end
+  end  
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   get '/signup',   to: 'users#new' 
   post '/signup',  to: 'users#create'
+  
   resources :users
 end
