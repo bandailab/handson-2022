c7ec5dcedb908cd94ac04afa9f62c62f98ffadf4
Shota Minegishi <smngs@mine-313.com>
2022-07-12 10:01:01 +0900

    [fix] add model validation for User.introduction
    * model の validation 機能を用いて，User.introduction に書き込める文字数を 170 文字に制限しました．

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
+  validates :introduction, presence: true, length: { maximum: 170 }
 end
